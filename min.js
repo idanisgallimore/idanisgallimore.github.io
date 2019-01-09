98998459992fd207ca2c7b51e717a9c9c5a91233
idanisg <idanisgallimore@gmail.com>
2019-01-09 14:19:01 -0500

    first commit

---
+++ min.js
@@ -0,0 +1,1 @@
+window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>350||document.documentElement.scrollTop>350){document.querySelector("header").style.top="0"}else{document.querySelector("header").style.top="-50px"}}
